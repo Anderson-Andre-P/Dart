2e1ccf2458a7475401ea2d12864ba36f1d24a66b
Anderson André Pereira Eleutério <aeleuterio@server01.foursys.local>
2022-05-23 15:26:58 -0300

    fix: tentando arrumar o exercicio 10

--- aula 03/Exercicios/010.dart
+++ aula 03/Exercicios/010.dart
@@ -6,9 +6,59 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  var numeros = [];
+  stdout.write("Digite um número: ");
+  double numero1 =
+      double.tryParse(stdin.readLineSync()!.replaceAll(',', '.')) ?? 0;
+
+  stdout.write("Digite um número: ");
+  double numero2 =
+      double.tryParse(stdin.readLineSync()!.replaceAll(',', '.')) ?? 0;
+
+  stdout.write("Digite um número: ");
+  double numero3 =
+      double.tryParse(stdin.readLineSync()!.replaceAll(',', '.')) ?? 0;
+
+  stdout.write("Digite um número: ");
+  double numero4 =
+      double.tryParse(stdin.readLineSync()!.replaceAll(',', '.')) ?? 0;
+
+  stdout.write("Digite um número: ");
+  double numero5 =
+      double.tryParse(stdin.readLineSync()!.replaceAll(',', '.')) ?? 0;
+
+  stdout.write("Digite um número: ");
+  double numero6 =
+      double.tryParse(stdin.readLineSync()!.replaceAll(',', '.')) ?? 0;
+
+  stdout.write("Digite um número: ");
+  double numero7 =
+      double.tryParse(stdin.readLineSync()!.replaceAll(',', '.')) ?? 0;
+
+  stdout.write("Digite um número: ");
+  double numero8 =
+      double.tryParse(stdin.readLineSync()!.replaceAll(',', '.')) ?? 0;
+
+  List numeros = [58, 13, 2, 9, 4];
+
   for (var i = 0; i <= 7; i++) {
-    numeros[i] = new Random();
+    if (numeros[i] == numero1) {
+      print("Você digitou um número igual ao da lista!");
+    } else if (numeros[i] == numero2) {
+      print("Você digitou um número igual ao da lista!");
+    } else if (numeros[i] == numero3) {
+      print("Você digitou um número igual ao da lista!");
+    } else if (numeros[i] == numero4) {
+      print("Você digitou um número igual ao da lista!");
+    } else if (numeros[i] == numero5) {
+      print("Você digitou um número igual ao da lista!");
+    } else if (numeros[i] == numero6) {
+      print("Você digitou um número igual ao da lista!");
+    } else if (numeros[i] == numero7) {
+      print("Você digitou um número igual ao da lista!");
+    } else if (numeros[i] == numero8) {
+      print("Você digitou um número igual ao da lista!");
+    } else {
+      print("Erro inesperado");
+    }
   }
-  print("${numeros}");
 }
